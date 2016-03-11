72d1a77e446c48d7d3e06bb95d6299a0d682e338
Alex Standiford <tstandiford@gmail.com>
2016-03-11 08:48:04 -0500

    Initial Push for Untappd API push

---
+++ untappd-api.php
@@ -0,0 +1,24 @@
+<?php
+
+class tasbb_ut{
+  public $ID = '107E2F6BA2ADE6A462D97976000782C1EC7A797D';
+  public $key = '1206E9C1093C50ADF259943FB1298B6C5E8F904E';
+  public $breweryID = 2663;
+  public $apiVersion = 'v4';
+  public function __construct(){
+    $this->breweryUrl = 'https://api.untappd.com/'.$this->apiVersion.'/brewery/info/'.$this->breweryID.'?client_id='.$this->ID.'&client_secret='.$this->key;
+  }
+};
+$tasbb_ut = new tasbb_ut;
+
+function tasbb_get_brewery_info(){
+  //global $tasbb_ut;
+  //$json = file_get_contents($tasbb_ut->breweryUrl);
+  $json = file_get_contents(plugin_dir_url(__FILE__).'test.json');
+  $obj = json_decode($json);
+  foreach($obj->response->brewery->beer_list->items as $beer){
+  var_dump($beer->beer);  
+  }
+}
+
+//tasbb_get_brewery_info();
