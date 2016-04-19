63448534f2e2fd6304c49ad19f5e5b7b30df71ba
Alex Standiford <tstandiford@gmail.com>
2016-04-18 21:59:43 -0400

    Added menu featured image to template

--- ebl-tv-menu-template.php
+++ ebl-tv-menu-template.php
@@ -11,18 +11,20 @@ $ebl_menu = new ebl_menu(4);
 ?>
 <header>
   <h1><?php echo get_the_title(); ?></h1>
+  <img src="<?php echo $ebl_menu->thumbnail; ?>">
   <h2><?php echo $ebl_menu->subheading; ?></h2>
   <p><?php echo $ebl_menu->beforeMenu; ?></p>
 </header>
 <ul>
 <?php
 $beers = new WP_Query($ebl_menu->args());
+$i = 1;
 if($beers->have_posts()) : while($beers->have_posts()) : $beers->the_post();
 $beer_width = 100 / (ceil($beers->found_posts / $ebl_menu->beersPerColumn));
 $beer_height = 100 / $ebl_menu->beersPerColumn;
 
 ?>
-	<li class="print-wrap" style="height: <?php echo $beer_height; ?>%; width:calc(<?php echo $beer_width; ?>% - 50px);">
+	<li class="<?php if($i > $ebl_menu->beersPerColumn){echo 'col-2';}; ?> print-wrap" style="height: <?php echo $beer_height; ?>%; width:calc(<?php echo $beer_width - 5; ?>% - 50px);">
       <aside <?php if($ebl_menu->filter['show_image'] == true){ echo 'class="has-img"'; }; ?>>
         <?php if($ebl_menu->filter['show_image'] == true){ ?><img src="<?php echo the_post_thumbnail_url();?>"><?php };?>
         <div class="beer-info">
@@ -51,7 +53,7 @@ $beer_height = 100 / $ebl_menu->beersPerColumn;
            </div>
 		</aside>
 	</li>
-<?php }; ?>
+<?php }; $i++; ?>
 <?php endwhile; endif; ?>
 </ul>
 <?php wp_reset_query(); ?>
