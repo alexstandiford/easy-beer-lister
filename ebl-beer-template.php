02020486383c89d9f6837c4eca6b3ace2d700141
Alex Standiford <tstandiford@gmail.com>
2016-04-21 06:46:55 -0400

    Added a ton of filters and actions to beer template page

--- ebl-beer-template.php
+++ ebl-beer-template.php
@@ -1,9 +1,15 @@
 <?php get_header();?>
 <?php if(have_posts()) : ?>
 <?php while(have_posts()) : the_post(); ?>
-	<div id="primary" class="content-heading-wrapper">
-        <div class="content-heading-overlay">
-            <div class="content-heading">
+<?php
+do_action('ebl_before_beer_wrapper');
+?>
+	<div id="primary" class="<?php echo apply_filters('ebl_beer_body_class', 'content-heading-wrapper'); ?>">
+     <?php do_action('ebl_before_beer_overlay'); ?>
+        <div class="<?php echo apply_filters('ebl_beer_heading_overlay_class', 'content-heading-overlay'); ?>">
+          <?php do_action('ebl_before_beer_heading'); ?>
+            <div class="<?php echo apply_filters('ebl_beer_heading_class', 'content-heading'); ?>">
+              <?php do_action('ebl_before_beer_title'); ?>
                 <h2><?php the_title();?></h2>
                 <?php if(function_exists('ebl_beer_info')){ebl_beer_info('style','h2',null,true);};?>
                 <?php if(function_exists('ebl_beer_is_on_tap')){
@@ -11,15 +17,18 @@
                 <h4 class="on-tap"><a href="<?php ebl_beer_info_url('availability',null,true) ?>">On Tap Now!</a></h4>
                 <?php };};?>
                 <hr>
-                <p class="categories">Availability:<?php if(function_exists('ebl_beer_info')){ebl_beer_info('availability','span');};?></p>
+                <p class="<?php echo apply_filters('ebl_beer_availability_class', 'availability'); ?>">Availability:<?php if(function_exists('ebl_beer_info')){ebl_beer_info('availability','span');};?></p>
             </div>
         </div>
     </div>
-    <div id="primary" class="ebl-primary-content-wrapper">
-        <div class="ebl-primary-content">
+    <div id="primary" class="<?php echo apply_filters('ebl_beer_content_wrapper', 'ebl-primary-content-wrapper'); ?>">
+      <?php do_action('ebl_before_beer_content'); ?>
+        <div class="<?php echo apply_filters('ebl_beer_content', 'ebl-primary-content'); ?>">
+          <?php do_action('ebl_before_beer_excerpt'); ?>
         <blockquote>
         <?php the_excerpt();?>
         </blockquote>
+          <?php do_action('ebl_before_beer_info'); ?>
         <?php if(function_exists('ebl_beer_info')){?>
           <?php if(ebl_beer_info_exists('ebl_untappd_url')){?>
           <a class="untappd-url btn" href="<?php ebl_beer_info('ebl_untappd_url');?>" target="blank">View on Untappd</a>
@@ -35,25 +44,28 @@
              <dd><?php ebl_beer_info('ebl_abv'); ?></dd>
            </div>
            <?php };
+            do_action('ebl_after_beer_abv');
             if(ebl_beer_info_exists('ebl_ibu')){?>
            <div>
            <dt>IBU:</dt>
              <dd><?php ebl_beer_info('ebl_ibu'); ?></dd>
            </div>
            <?php };
+            do_action('ebl_after_beer_ibu');
             if(ebl_beer_info_exists('ebl_og')){?>
            <div>
            <dt>Original Gravity:</dt>
              <dd><?php ebl_beer_info('ebl_og'); ?></dd>
            </div>
            <?php };
+            do_action('ebl_after_beer_og');
             if(ebl_beer_info_exists('pairing')){?>
            <div>
              
            <dt>Pairs With:</dt>
          <?php ebl_beer_info('pairing','dd'); ?>
            </div>
-           <?php  }; ?>
+           <?php  }; do_action('ebl_after_beer_pairing'); ?>
          </dl>
 					</div>
 					<span class="ebl-row"></span>
@@ -66,9 +78,11 @@
         }
         ?>
         </div>
+      <?php do_action('ebl_after_beer_info'); ?>
     </div>
  <?php
 endwhile;
 ?>
 <?php endif; ?>
+<?php do_action('ebl_before_beer_footer'); ?>
 <?php get_footer();?>
