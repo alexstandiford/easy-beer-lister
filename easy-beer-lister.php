2c51bb0aba9e4629a6e1139fe6a5d03963030b0a
Alex Standiford <tstandiford@gmail.com>
2016-06-27 06:33:14 -0400

    Final update to 1.21

--- easy-beer-lister.php
+++ easy-beer-lister.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Easy Beer Lister
 Description: Manage Your Beers, Beer Pairings, and What's on Tap Easily
-Version:     1.20
+Version:     1.21
 Author:      Alex Standiford
 Author URI:  http://www.easybeerlister.com
 */
