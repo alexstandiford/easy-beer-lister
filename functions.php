8298155ff06b8afe3cf4624c43b51f15acaeb7c2
Alex Standiford <a@alexstandiford.com>
2018-04-05 10:52:12 -0400

    Added composer support

--- functions.php
+++ functions.php
@@ -15,7 +15,7 @@ if(!defined('ABSPATH')) exit;
  * I've always wanted to make a function called this.
  * Gets the current beer object
  */
-function ebl_get_beer($id = null){
+function get_beer($id = null){
   if($id) return new beer($id);
   global $ebl_beer;
 
@@ -26,7 +26,7 @@ function ebl_get_beer($id = null){
  * Loads the current query into the file.
  * @return beerList
  */
-function ebl_get_beer_list(){
+function get_beer_list(){
   global $ebl_beer_list;
 
 
